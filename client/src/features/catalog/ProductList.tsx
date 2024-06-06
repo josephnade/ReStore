d8dc588c16f78899d7c6a9f2087cc682f5623d30
Yusuf Akin <yusuf.akn2017@hotmail.com>
2024-06-06 11:11:55 +0300

    End of Section 3

---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,18 @@
+import { Grid } from "@mui/material";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+    products: Product[]
+}
+
+export default function ({ products }: Props) {
+    return (
+        <Grid container spacing={4} >
+
+            {products.map(product => {
+                return <Grid item xs={4} key={product.id}><ProductCard product={product}></ProductCard></Grid>
+            })}
+        </Grid>
+    );
+}
